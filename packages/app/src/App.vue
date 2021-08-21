c3242111cffc042617d16f5c9920e88d7a3f8477
cn3lfs <lour6498@gmail.com>
2021-08-21 10:28:43 +0800

    mono repo

---
+++ packages/app/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import { concatArray } from "common";
+export default {
+  setup() {
+    return {
+      array: concatArray(),
+    };
+  },
+};
+</script>
+<template>
+  <h1>{{ array }}</h1>
+</template>
---
+++ packages/common/src/index.ts
@@ -0,0 +1,4 @@
+import { concat } from "lodash-es";
+export const concatArray = function () {
+  return concat([1], [2]);
+};
